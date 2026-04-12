433e9243944ea80fb0f187cb01768209a0dca642
wachirapong-n <wachirapong_n@cmu.ac.th>
2026-04-12 22:19:50 +0700

    add underline animation on navbar

--- src/components/custom/navbar-menu.tsx
+++ src/components/custom/navbar-menu.tsx
@@ -26,31 +26,47 @@ export default function NavbarMenu({
   return (
     <>
       <div className="flex flex-1 gap-5 items-center relative z-10">
-        <div className="flex">
-          <Link href={`/`}>
-            <img
-              src={urlFor(logo).url()}
-              className="object-cover h-8 w-8 xl:w-10 xl:h-10 2xl:w-12 2xl:h-12"
-              alt="logo"
-            />
-          </Link>
-        </div>
         <Link href={`/`}>
-          <div className="items-center gap-1 font-medium transition-colors py-2 xl:text-lg 2xl:text-xl group-hover/nav:text-primary hover:underline">
+          <img
+            src={urlFor(logo).url()}
+            className="object-cover h-8 w-8 xl:w-10 xl:h-10 2xl:w-12 2xl:h-12"
+            alt="logo"
+          />
+        </Link>
+        <Link href={`/`} className="font-medium group/text">
+          <div className="relative inline-block items-center gap-1 font-medium transition-colors py-2 xl:text-lg 2xl:text-xl group-hover/nav:text-primary ">
             {websiteName}
+            <span
+              className="
+        absolute left-0 bottom-1 h-0.5 w-full bg-current
+        scale-x-0 origin-left
+        transition-transform duration-300 ease-out
+        group-hover/text:scale-x-100
+        group-hover/text:text-primary
+      "
+            />
           </div>
         </Link>
       </div>
       <div className="hidden xl:flex gap-5 justify-end xl:text-lg 2xl:text-xl items-center">
-        <Link href={`/`} className="font-medium transition-colors">
-          <div className="items-center gap-1 font-medium group-hover/nav:text-primary relative z-10 hover:underline px-2">
+        <Link href={`/`} className="font-medium group/text">
+          <div className="relative inline-block group-hover/nav:text-primary">
             หน้าแรก
+            <span
+              className="
+        absolute left-0 -bottom-1 h-0.5 w-full bg-current
+        scale-x-0 origin-left
+        transition-transform duration-300 ease-out
+        group-hover/text:scale-x-100
+        group-hover/text:text-primary
+      "
+            />
           </div>
         </Link>
         {navData?.map((category) => (
           <div
             key={category._id}
-            className="group relative flex items-center h-full group-hover/nav:text-primary hover:underline px-2"
+            className="group relative flex items-center h-full group-hover/nav:text-primary px-2"
           >
             <Link
               href={`/${category.slug.current}`}
@@ -61,9 +77,20 @@ export default function NavbarMenu({
                   setOpen(open === category._id ? null : category._id);
                 }
               }}
-              className="flex items-center gap-1 font-medium transition-colors "
+              className="flex items-center gap-1 font-medium transition-colors"
             >
-              {category.title}
+              <span className="relative inline-block">
+                {category.title}
+                <span
+                  className="
+            absolute left-0 -bottom-1 h-0.5 w-full bg-current
+            scale-x-0 origin-left
+            transition-transform duration-300 ease-out
+            group-hover:scale-x-100
+          "
+                />
+              </span>
+
               <svg
                 className="w-4 h-4 transition-transform group-hover:rotate-180"
                 fill="none"
@@ -78,9 +105,10 @@ export default function NavbarMenu({
                 />
               </svg>
             </Link>
+            <div className="absolute left-0 top-full w-full h-4" />
             <div
               className={cn(
-                "absolute z-50 right-0 top-full w-48 rounded-lg border border-gray-100 bg-white p-2 shadow-xl ",
+                "absolute z-50 right-0 top-full translate-y-4 w-48 rounded-lg border border-gray-100 bg-white p-2 shadow-xl",
                 "hidden xl:group-hover:block",
                 open === category._id && "block xl:hidden",
               )}
